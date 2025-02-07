6ae4542588a1031f0e1b20e243c3670f68cbd178
Jack Xu <xulingcom@126.com>
2025-02-07 10:11:35 +0800

    Update update.ps1

--- update.ps1
+++ update.ps1
@@ -38,6 +38,12 @@ Remove-Item -Path .\scoop-sysinternals -Recurse -Force
 Get-ChildItem -Recurse -Path .\bucket | ForEach-Object -Process {
     $content = Get-Content $_.FullName
 
+    # DBeaver，not debaver-ea
+    $content = $content -replace 'https?://dbeaver\.io/files/([\d\.]+)/', 'https://mirrors.nju.edu.cn/github-release/dbeaver/dbeaver/$1/'
+
+    # git
+    $content = $content -replace 'https?://github.com/git-for-windows/git/releases/download', 'https://mirrors.huaweicloud.com/git-for-windows'
+
     # GitHub Releases
     $content = $content -replace '(https?://github\.com/.+/releases/.*download)', 'https://ghproxy.cc/$1'
 
